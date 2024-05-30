ecd7b28d940df369dfe463fde8ccf8f91e76382f
Thoposaurus <149897944+Thoposaurus@users.noreply.github.com>
2024-05-30 09:29:16 -0400

    Creation of National Park object

---
+++ app/src/main/java/com/example/locationapp/NationalPark.java
@@ -0,0 +1,21 @@
+package com.example.locationapp;
+
+import java.time.LocalDate;
+
+public class NationalPark {
+    private final String parkName;
+    private final boolean hasVisited = false;
+    private final boolean hasCompleted = false;
+    private final int numOfVisits = 0;
+    private final LocalDate dateOfLastVisit = null;
+    private final LocalDate dateOfLastCompletion = null;
+    //private final String parkNotes;
+
+
+
+
+    public NationalPark(String parkName) {
+        this.parkName = parkName;
+    }
+
+}
