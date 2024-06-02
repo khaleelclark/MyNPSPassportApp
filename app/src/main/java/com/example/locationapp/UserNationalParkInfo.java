3a340e5a3d096582a1d8856f3e113aeacae7c64d
Thoposaurus <149897944+Thoposaurus@users.noreply.github.com>
2024-06-02 12:28:09 -0400

    Creation of User and Default National Park objects

--- app/src/main/java/com/example/locationapp/UserNationalParkInfo.java
+++ app/src/main/java/com/example/locationapp/UserNationalParkInfo.java
@@ -2,20 +2,31 @@ package com.example.locationapp;
 
 import java.time.LocalDate;
 
-public class NationalPark {
+public class UserNationalParkInfo {
     private final String parkName;
     private final boolean hasVisited = false;
     private final boolean hasCompleted = false;
     private final int numOfVisits = 0;
     private final LocalDate dateOfLastVisit = null;
     private final LocalDate dateOfLastCompletion = null;
-    //private final String parkNotes;
+    private String parkNotes;
 
 
 
 
-    public NationalPark(String parkName) {
+    public UserNationalParkInfo(String parkName) {
         this.parkName = parkName;
     }
 
+    public String getParkName() {
+        return parkName;
+    }
+
+    public String getParkNotes() {
+        return parkNotes;
+    }
+
+    public void setParkNotes(String parkNotes) {
+        this.parkNotes = parkNotes;
+    }
 }
