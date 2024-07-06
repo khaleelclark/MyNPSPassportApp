4cf990a5b85a8c8287178f8116e9c8ede69d4ee9
Zindel <khaleezindel@gmail.com>
2024-07-06 18:17:47 -0400

    created database functionality

--- app/src/main/java/com/example/locationapp/MainActivity.java
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.locationapp;
 
-import android.content.Intent;
 import android.view.View;
-import android.widget.*;
+import android.widget.EditText;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.room.Room;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //make private
     public static ViewModel viewModel;
+    private EditText usernameEditText;
+    private EditText firstNameEditText;
+    private Button registerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString();
+            String firstName = firstNameEditText.getText().toString();
+            registerUser(username, firstName);
+
+            System.out.println(username + ' ' + firstName);
+        });
+
+
         viewModel = new ViewModelProvider(this).get(ViewModel.class);
 
         viewModel.getAllNationalParks().observe(this, nationalParks -> {
@@ -34,9 +47,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void collectUserParkInfo(View view) {
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_set_park_data);
     }
     public void changeToSignUpScreen(View view) {
         setContentView(R.layout.activity_login);
+    }
+
+    private void registerUser (String username, String firstName){
+        //save inputs to database
     }
 }
